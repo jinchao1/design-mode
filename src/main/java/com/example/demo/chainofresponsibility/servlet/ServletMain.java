ec36d2b2d2e8eafc94708b5324baf0d3bc23281b
jinchao1 <13718956081@139.com>
2022-11-04 22:14:26 +0800

    设计模式之责任链模式

---
+++ src/main/java/com/example/demo/chainofresponsibility/servlet/ServletMain.java
@@ -0,0 +1,150 @@
+package com.example.demo.chainofresponsibility.servlet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Auther: jinchao
+ * @Date: 2022/11/2 - 21:08
+ * @Description:  设计模式之责任链模式
+ * @version: 1.0
+ */
+public class ServletMain {
+    public static void main(String[] args) {
+        Msg msg = new Msg();
+        msg.setMsg("大家好:),<script>,欢迎访问mashibing.com，大家都是996");
+
+        //处理msg
+//        new HTMLFilter().doFilter(msg);
+//        new SensitiveFilter().doFilter(msg);
+
+//        List<Filter> filters = new ArrayList<>();
+//        filters.add(new HTMLFilter());
+//        filters.add(new SensitiveFilter());
+//        for (Filter filter : filters) {
+//            filter.doFilter(msg);
+//        }
+
+        FilterChain filterChain = new FilterChain();
+        filterChain.addFilter(new HTMLFilter())
+                .addFilter(new SensitiveFilter());
+
+
+        FilterChain filterChain2 = new FilterChain();
+        filterChain2.addFilter(new FaceFilter())
+                .addFilter(new URLFilter());
+        filterChain.addFilter(filterChain2);
+
+        filterChain.doFilter(msg);
+        System.out.println(msg.getMsg());
+
+    }
+}
+
+class Msg{
+    String name;
+    String msg;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return "Msg{" +
+                "name='" + name + '\'' +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+}
+
+interface Filter{
+    boolean doFilter(Request request,Response response);
+}
+
+
+class HTMLFilter implements Filter {
+    @Override
+    public boolean doFilter(Request request,Response response) {
+        return false;
+    }
+}
+
+class Request{
+    String str;
+}
+
+class Response{
+    String str;
+}
+
+class SensitiveFilter implements Filter {
+    @Override
+    public boolean doFilter(Request request,Response response) {
+        /*String m = msg.getMsg();
+        m = m.replace("996","955");
+        msg.setMsg(m);*/
+        return false;
+    }
+}
+
+class FaceFilter implements Filter {
+    @Override
+    public boolean doFilter(Request request,Response response) {
+        /*String m = msg.getMsg();
+        m = m.replace(":)","^V^");
+        msg.setMsg(m);*/
+        return false;
+    }
+}
+
+class URLFilter implements Filter {
+    @Override
+    public boolean doFilter(Request request,Response response) {
+        /*String m = msg.getMsg();
+        m = m.replace("mashibing.com","http://www.mashibing.com");
+        msg.setMsg(m);*/
+        return false;
+    }
+}
+
+class FilterChain implements Filter {
+    List<Filter> filters = new ArrayList<>();
+
+    public FilterChain addFilter(Filter f){
+        filters.add(f);
+        return this;
+    }
+
+    public boolean doFilter(Request request,Response response){
+        return false;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
