162d25408ecbed4ce7724ec3585b58b8aaf894b8
jinchao1 <13718956081@139.com>
2022-11-23 22:30:54 +0800

    设计模式之代理模式

--- src/main/java/com/example/demo/proxy/v08/Tank.java
+++ src/main/java/com/example/demo/proxy/v08/Tank.java
@@ -14,7 +14,11 @@ import java.util.Random;
  *  v06:代理有各种类型
  *  问题：如何实现代理的各种组合？继承？Decorator?
  *  v07:代理的对象改成Movable类型—越来越像 decorator 了
- *
+ *  v08:如果有stop方法需要代理...
+ *  如果想让LogProxy可以重用，不仅可以代理Tank，还可以代理任何其他可以代理的类型 Object
+ *  （毕竟日志记录，时间计算是很多方法都需要的东西），这时该怎么做呢？
+ *  分离代理行为与被代理对象
+ *  使用jdk的动态代理
  */
 public class Tank implements Movabel {
 
---
+++ src/main/java/com/example/demo/spring/v2/TimeProxy.java
@@ -0,0 +1,21 @@
+package com.example.demo.spring.v2;
+
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class TimeProxy {
+
+    @Before("execution (void com.example.demo.spring.v2.Tank.move())")
+    public void before() {
+        System.out.println("method start.." + System.currentTimeMillis());
+    }
+
+    @After("execution (void com.example.demo.spring.v2.Tank.move())")
+    public void after() {
+        System.out.println("method stop.." + System.currentTimeMillis());
+    }
+
+}
