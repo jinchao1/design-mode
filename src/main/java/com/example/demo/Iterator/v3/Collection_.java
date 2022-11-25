bc1ba0947af758655a591e82d0d8df3b6f250232
jinchao1 <13718956081@139.com>
2022-11-25 22:44:41 +0800

    设计模式之Iterator

---
+++ src/main/java/com/example/demo/Iterator/v3/Collection_.java
@@ -0,0 +1,6 @@
+package com.example.demo.Iterator.v3;
+
+public interface Collection_ {
+    void add(Object o);
+    int size();
+}
---
+++ src/main/java/com/example/demo/Iterator/v5/Iterator_.java
@@ -0,0 +1,7 @@
+package com.example.demo.Iterator.v5;
+
+public interface Iterator_ {
+    boolean hasNext();
+
+    Object next();
+}
