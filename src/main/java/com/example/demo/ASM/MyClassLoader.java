11bda694b7de16f1c8e75248a8235cb81bef7f07
jinchao1 <13718956081@139.com>
2022-11-26 22:47:37 +0800

    ASM

---
+++ src/main/java/com/example/demo/ASM/MyClassLoader.java
@@ -0,0 +1,9 @@
+package com.example.demo.ASM;
+
+class MyClassLoader extends ClassLoader {
+    public Class defineClass(String name, byte[] b) {
+        return defineClass(name, b, 0, b.length);
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/ASM/TimeProxy.java
@@ -0,0 +1,8 @@
+package com.example.demo.ASM;
+
+public class TimeProxy {
+
+    public static void before() {
+        System.out.println("before ...");
+    }
+}
